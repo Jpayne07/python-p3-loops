57699462a8357c6b337bab67fa43e1e12847daae
Jacob <jacobpayne007@gmail.com>
2024-09-17 07:56:16 -0500

    sone

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i > 0 :
+        print(f'{i}\n')
+        print("Happy New Year!")
+        i-=1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squaredList = [integer * integer for integer in int_list]
+    return squaredList
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1 ,101):
+        if i % 5 ==0 and i % 3==0:
+            print('FizzBuzz')
+        elif i % 5==0:
+            print('Buzz')
+        elif i % 3==0:
+            print('Fizz')
+        else:
+            print(i)
+fizzbuzz()
